e32c36e60a353b8f697a013132b12edb2ef0a081
itherohit <rohitshan751@gmail.com>
2020-05-09 21:26:22 +0530

    Category Added

--- app/controllers/menuitems_controller.rb
+++ app/controllers/menuitems_controller.rb
@@ -8,5 +8,8 @@ class MenuitemsController < ApplicationController
         # render plain: "done"
         # item =  MenuItem.create!(item_name: "Oreo coffee", img_url:"https://cafe-management.s3-ap-northeast-1.amazonaws.com/Menu+Images/thickshakes/oreo.jpg", price:"220",item_desciption:"An addictive oreo crumble shake blened with cocoa.",category_id:"16")       
         # render plain: item.id
+        c = Category.all[1]
+        # item = c.menuitems
+        render plain: c.menu_items[0].item_name
         end
 end
